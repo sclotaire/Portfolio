9c0f55152f4848b7611bdfed6cdeb3b089ee0d32
sclotaire <s.clotaire@hotmail.co.uk>
2024-05-30 18:18:06 +0100

    hero complete

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#44164c] bgitems-center justify-between p-24 container mx-auto">
+    <main className="flex min-h-screen flex-col bg-[#EEEDEB] bgitems-center justify-between p-24 container mx-auto">
       <Hero/>
     </main>
   );
